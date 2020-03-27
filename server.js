116a9457b340224d0957d9cec36308ec872ac5fa
Paul Schlueter <paul@paulschlueter.com>
2020-03-26 23:00:26 -0500

    Node Server Returning Excel File

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const { parse } = require('querystring');
+const path = require('path')
 const app = express()
 const port = 8080
 
 app.post('/file-upload', (request, response) => {
   if (request.method === 'POST') {
     collectRequestData(request, result => {
-        console.log(JSON.stringify(result));
+        console.log('Request Body:')
+        console.log(result);
     });
   }
 
   response.set('Access-Control-Allow-Origin', '*')
-  response.send('Hello from Express!')
+  const filePath = path.join(__dirname, 'sample.xlsx')
+  response.sendFile(filePath)
 })
 
 app.listen(port, (err) => {
