99bd93d6c8612a8c10259fff9ec9b1f29d1f22ae
Paul Schlueter <paul@paulschlueter.com>
2020-03-26 22:44:11 -0500

    Log File Contents In Node Server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const { parse } = require('querystring');
 const app = express()
 const port = 8080
 
 app.post('/file-upload', (request, response) => {
+  if (request.method === 'POST') {
+    collectRequestData(request, result => {
+        console.log(JSON.stringify(result));
+    });
+  }
+
   response.set('Access-Control-Allow-Origin', '*')
   response.send('Hello from Express!')
 })
@@ -14,3 +21,19 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 })
+
+function collectRequestData(request, callback) {
+  const FORM_URLENCODED = 'multipart/form-data';
+  if(request.headers['content-type'] === FORM_URLENCODED) {
+      let body = '';
+      request.on('data', chunk => {
+          body += chunk.toString();
+      });
+      request.on('end', () => {
+          callback(parse(body));
+      });
+  }
+  else {
+      callback(null);
+  }
+}
